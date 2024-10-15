09aad3b1ea9813f73cef497ecf91546f23574926
Riddhi Patel <riddhi2002@gmail.com>
2024-10-15 15:55:51 +0530

    testcommit

--- AzureWebRole.cs
+++ AzureWebRole.cs
@@ -9,6 +9,7 @@ namespace SitefinityWebApp
     /// Windows Azure looks for RoleEntryPoint inheritors only in the web role assembly and that is why this class is defined here.
     /// The functionality goes into the base class - <see cref="AzureWebRoleBase"/>.
     /// </remarks>
+    /// comment chnages by Riddhiii
     public class AzureWebRole : AzureWebRoleBase
     {
     }
